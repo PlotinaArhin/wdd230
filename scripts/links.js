56ff0484e9fbc51b8593ec1727f1a7c5b060b198
PlotinaArhin <arhinplotina@gmail.com>
2024-07-23 21:47:18 +0000

    made changes

---
+++ scripts/links.js
@@ -0,0 +1,44 @@
+const baseURL = "https://github.com/PlotinaArhin/wdd230";
+const linksURL = "data/links.json";
+const ulToLinks = document.querySelector("#linksList");
+
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    if (response.ok) {
+        const data = await response.json();
+        //console.table(data.weeks);
+        displayLinks(data.weeks);
+    }
+}
+
+
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+
+        let li = document.createElement('li');
+        li.append(week.week);
+        li.append(": ");
+
+        week.links.forEach((link) => {
+            let a = document.createElement('a');
+            a.setAttribute('href', link.url);
+            a.setAttribute('title', link.title);
+            a.setAttribute('target', "_blank");
+            a.textContent = link.title;
+
+            li.appendChild(a);
+            li.append(" | ");
+        })
+
+        ulToLinks.appendChild(li);
+
+
+
+    });
+}
+
+
+
+getLinks();
